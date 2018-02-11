476c27a59f0861145284fb998d23d26547cb1cf6
Bradibarus <bradibarus@gmail.com>
2018-02-11 23:56:50 +0300

    package rename

--- src/main/java/xyz/bradibarus/bradibarious/controller/AccountController.java
+++ src/main/java/xyz/bradibarus/bradibarious/controller/AccountController.java
@@ -9,9 +9,8 @@ import xyz.bradibarus.bradibarious.service.AccountService;
 import javax.annotation.security.RolesAllowed;
 import java.security.Principal;
 
-@Secured({"ROLE_USER"})
 @RestController
-@RequestMapping(value = "/account")
+@RequestMapping(value = "/account/{userId}")
 public class AccountController {
     @Autowired
     private final AccountService accountService;
@@ -20,14 +19,23 @@ public class AccountController {
         accountService = service;
     }
 
+    @Secured({"ROLE_USER"})
     @RequestMapping(method = RequestMethod.GET)
-    String testGet() {
-        return "You are successfully getting response from AccountController";
+    Account testGet(Principal principal) {
+        return accountService.findByUsername(principal.getName()).get();
     }
 
+    @Secured({"ROLE_USER"})
     @RequestMapping(method = RequestMethod.DELETE)
     Account deleteUser(Principal principal, @RequestBody String password){
         return accountService.delete(new Account(principal.getName(), password))
             .orElseThrow(()->new UserNotFoundException(principal.getName()));
     }
+
+    @Secured({"ROLE_GUEST"})
+    @RequestMapping(method = RequestMethod.POST)
+    Account addUser(@RequestBody String password, @PathVariable String userId) {
+        return accountService.add(new Account(userId, password));
+    }
+
 }
